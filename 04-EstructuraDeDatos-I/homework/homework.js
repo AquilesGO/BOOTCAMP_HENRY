96ff7380e058b380a275795052c0e4d4a280391d
Aquileo <agutov@gmail.com>
2022-10-17 11:18:33 -0500

    guardado rutinario

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -12,14 +12,56 @@ function nFactorial(n) {
 
 // EJERCICIO 2
 function nFibonacci(n) {
+// si me pide lo que hay en una posición negativa retorno null.
+  if (n < 0) return null;
+// si me pide lo que hay en la posición 0, retorno 0.
+// si me pide lo que hay en la posición 1, retorno 1.
+if (n === 0 || n === 1) return n;
+// en cualquier otro cas, retorno la suma de lo que haya
+// en la posición (n-1) y lo que haya en la posición (n-2)
+return nFibonacci (n - 1) + nFibonacci (n - 2);
 
-   
 }
 
 // EJERCICIO 3
-function Queue() {
-   // this.cola: []
+// function Queue() {
+//    this.queue = [];
+// }
+
+// Queue.prototype.enqueue = function (element){
+//    this.queue.push(element);
+// };
+
+// Queue.prototype.dequeue = function (){
+//    return this.queue.shift();
+// };
+
+// Queue.prototype.size = function (){
+//    return this.queue.length;
+// };
+
+// var laFilaDelBanco = new Queue();
+// console.log(laFilaDelBanco.size());
+
+
+//------------Mismo ejercicio con class:-----------
+class Queue {
+   constructor(){
+      this.queue = [];
+   }
+enqueue(element) {
+   this.queue.push(element);   
 }
+
+dequeue(){
+   return this.queue.shift();
+}
+
+size(){
+   return this.queue.length;
+}
+}
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
